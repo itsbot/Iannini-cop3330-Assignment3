6d1a3d99422d6ed1cecfb861c7fea5805a0f230c
itsbot <patrickiannini@knights.ucf.edu>
2021-10-11 19:30:57 -0400

    Completed exercise 45

---
+++ src/main/java/ex44/ex44.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Patrick Iannini
+ */
+package ex44;
+import com.google.gson.Gson;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class ex44 {
+    public static void main(String[] args) {
+        try {
+            Gson gson = new Gson();
+            Reader in = Files.newBufferedReader(Paths.get("src/main/java/ex44/exercise44_input.json"));
+
+
+
+
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
---
+++ src/main/java/ex45/ex45.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Patrick Iannini
+ */
+package ex45;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class ex45 {
+    public static void main(String[] args) throws IOException {
+        //File in = new File("src/main/java/ex45/exercise45_input.txt");
+        FileWriter fw = new FileWriter("src/main/java/ex45/exercise45_output.txt");
+
+
+        String text = Files.readString(Path.of("src/main/java/ex45/exercise45_input.txt"));
+        System.out.println(text+"\n");
+        String out = text.replaceAll("utilize", "use");
+        System.out.println(out);
+        fw.write(out);
+
+        fw.close();
+    }
+}
